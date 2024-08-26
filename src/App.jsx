73d0d3cc76ccaaaafa46b395d8e6c46980e7f1c7
liam <liam@nehoray.net>
2024-08-26 15:24:18 +0200

    Finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,52 @@
-/* eslint-disable react/prop-types */
 import { useState } from 'react'
 import Form from './components/form'
 import './App.css'
+import './components/form.css'
 
 function App() {
   const [submittedForm, setSubmittedForm] = useState(false)
-
-  const [formResults, setFormResults] = useState({
-    'name': ''
-
+  const [results, setResults] = useState({
+    'name': '',
+    'email': '',
+    'phone': '',
+    'school': '',
+    'graduation': '',
+    'company': '',
   })
 
-  const submitForm = () => {
+
+  const submitForm = (formResults) => {
+    // const newResults = {...formResults}
+    setResults(formResults)
     setSubmittedForm(true)
+  }
+
+  const editForm = () => {
+    setSubmittedForm(false)
   }
 
   if (submittedForm) {
     return (
-      <p>submitted form</p>
+      <>
+        <div className='form_container'>
+          <h2>info:</h2>
+          <p>{results.name}</p>
+          <p>{results.email}</p>
+          <p>{results.phone}</p>
+          <h2>Education</h2>
+          <p>{results.school}</p>
+          <p>{results.graduation}</p>
+          <h2>Work Experience</h2>
+          <p>{results.company}</p>
+          <button onClick={editForm}>Edit CV</button>
+        </div>
+      </>
+
     )
   } else {
     return (
       <>
-        <Form submitForm={submitForm}/>
+        <Form submitForm={submitForm} results={results}/>
       </>
     )
   }
--- src/components/inputs.jsx
+++ src/components/inputs.jsx
@@ -1,8 +1,17 @@
-export default function Input({ type='text', label='', value='',}) {
+import { useState } from "react";
+
+export default function Input({ type = 'text', label = '', Inputvalue, name = '', updateResults }) {
+    const [value, setValue] = useState(Inputvalue);
     return (
         <>
             <label>{label}</label>
-            <input type={type} value={value}/>
+            <input type={type}
+                value={value}
+                name={name}
+                onChange={(event) => {
+                    setValue(event.target.value);
+                    updateResults(name, event.target.value)
+                }} />
         </>
     )
 }
